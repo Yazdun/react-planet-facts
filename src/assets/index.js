76f246dc3d7df6b22403c3487c15513ed0cf6c04
Yazdun <yazduntube@gmail.com>
2022-05-26 16:17:33 +0430

    plants : created data structure

---
+++ src/assets/index.js
@@ -0,0 +1,39 @@
+// EARTH ASSETS
+export { PlanetEarth } from './planet-earth'
+export { PlanetEarthInernal } from './planet-earth-internal'
+export { GeologyEarth } from './geology-earth'
+
+// JUPITER ASSETS
+export { PlanetJupiter } from './planet-jupiter'
+export { PlanetJupiterInernal } from './planet-jupiter-internal'
+export { GeologyJupiter } from './geology-jupiter'
+
+// MARS ASSETS
+export { PlanetMars } from './planet-mars'
+export { PlanetMarsInernal } from './planet-mars-internal'
+export { GeologyMars } from './geology-mars'
+
+// MERCURY ASSETS
+export { PlanetMercury } from './planet-mercury'
+export { PlanetMercuryInernal } from './planet-mercury-internal'
+export { GeologyMercury } from './geology-mercury'
+
+// NEPTUNE ASSETS
+export { PlanetNeptune } from './planet-neptune'
+export { PlanetNeptuneInernal } from './planet-neptune-internal'
+export { GeologyNeptune } from './geology-neptune'
+
+// SATURN ASSETS
+export { PlanetSaturn } from './planet-saturn'
+export { PlanetSaturnInernal } from './planet-saturn-internal'
+export { GeologySaturn } from './geology-saturn'
+
+// URANUS ASSETS
+export { PlanetUranus } from './planet-uranus'
+export { PlanetUranusInernal } from './planet-uranus-internal'
+export { GeologyUranus } from './geology-uranus'
+
+// VENUS ASSETS
+export { PlanetVenus } from './planet-venus'
+export { PlanetVenusInernal } from './planet-venus-internal'
+export { GeologyVenus } from './geology-venus'
---
+++ src/data/index.js
@@ -0,0 +1,332 @@
+import {
+  PlanetEarth,
+  PlanetEarthInernal,
+  GeologyEarth,
+  PlanetJupiter,
+  PlanetJupiterInernal,
+  GeologyJupiter,
+  PlanetMars,
+  PlanetMarsInernal,
+  GeologyMars,
+  PlanetMercury,
+  PlanetMercuryInernal,
+  GeologyMercury,
+  PlanetNeptune,
+  PlanetNeptuneInernal,
+  GeologyNeptune,
+  PlanetSaturn,
+  PlanetSaturnInernal,
+  GeologySaturn,
+  PlanetUranus,
+  PlanetUranusInernal,
+  GeologyUranus,
+  PlanetVenus,
+  PlanetVenusInernal,
+  GeologyVenus,
+} from '../assets'
+
+export const data = [
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+
+  {
+    name: '',
+    overview: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarth,
+    },
+    structure: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+    },
+    geology: {
+      info: '',
+      sourceUrl: '',
+      svg: PlanetEarthInernal,
+      png: GeologyEarth,
+    },
+    details: [
+      {
+        title: 'rotation time',
+        info: '',
+      },
+      {
+        title: 'revolution time',
+        info: '',
+      },
+      {
+        title: 'radius',
+        info: '',
+      },
+      {
+        title: 'average temp.',
+        info: '',
+      },
+    ],
+  },
+]
