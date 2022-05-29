da2d5addef79422da01500a2e81548203ef8bd96
Yazdun <yazduntube@gmail.com>
2022-05-29 15:22:49 +0430

    desktop tabs : completed

---
+++ src/components/desktopTabs/framer.js
@@ -0,0 +1,8 @@
+export const framer_button = {
+  initial: { scale: 0 },
+  exit: { scale: 0 },
+  animate: { scale: 1 },
+  transition: {
+    delay: 0.2,
+  },
+}
--- src/components/desktopTabs/index.jsx
+++ src/components/desktopTabs/index.jsx
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom'
 import { pathnames } from './data'
+import { usePlanet } from '../../hooks'
 import css from './styles.module.css'
+import { planets } from '../../data'
+import cn from 'classnames'
 
-export const DesktopTabs = ({ planet = 'earth', color = '#6D2ED5' }) => {
+export const DesktopTabs = ({ planet = 'earth' }) => {
+  const { currentPlanet, currentTab } = usePlanet()
+
+  const color = planets.find(item => item.name === currentPlanet)?.color
+
   return (
     <ul className={css.list}>
       {pathnames.map(item => {
         const { path, number } = item
+        const isActive = currentTab === path
         return (
           <li className={css.item}>
-            <Link className={css.pathname} to={`/${planet}/${path}`}>
+            <Link
+              className={cn(css.pathname, isActive && css.disable)}
+              to={`/${planet}/${path}`}
+              style={{ backgroundColor: isActive && color }}
+            >
               <span className={css.number}>{number}</span>
               {path}
             </Link>
